cad599f7c4b87cdc70453f747814c07d9696ee7a
Soledad <daniela.cabrera.tapia@udla.edu.ec>
2023-04-18 21:20:03 -0500

    ok

---
+++ ejercicio19.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+   int num, i, primo = 1;
+
+   printf("Ingrese un numero entero: ");
+   scanf("%d", &num);
+
+   for (i = 2; i <= num / 2; i++) {
+      if (num % i == 0) {
+         primo = 0;
+         break;
+      }
+   }
+
+   if (primo == 1) {
+      printf("%d es un numero primo\n", num);
+   } else {
+      printf("%d no es un numero primo\n", num);
+   }
+
+   return 0;
+}
