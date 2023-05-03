6636658b711edd431b0786b708ebd93d7561ea35
Soledad <daniela.cabrera.tapia@udla.edu.ec>
2023-05-03 15:08:00 -0500

    primer intento del codigo

--- pruebaprogresouno.c
+++ pruebaprogresouno.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <conio.h>
 
+int main (){
+    
+    int x;
+    int i;
+
+    while (x=!0)
+    {
+        printf("ingrese numero");
+        scanf ("%d", &x);
+
+        if (x<0)
+        {
+        i=i+1;
+        }
+    }
+    printf("hay:&d numero negativos",i);
+
+    return 0;
+}
