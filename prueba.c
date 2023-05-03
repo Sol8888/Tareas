42cf4d3cd787a27ef5c51b20f8280fa577fcc122
Soledad <daniela.cabrera.tapia@udla.edu.ec>
2023-05-03 15:27:48 -0500

    prueba primer prograso

--- prueba.c
+++ prueba.c
@@ -1,10 +1,13 @@
+//Construya un programa que lea de forma continua números introducidos por el usuario y vaya contando los números negativos. El programa se debe detener cuando el usuario ingrese el número cero.//
+
+
 #include <stdio.h>
 #include <conio.h>
 
 int main (){
     
-    int x;
-    int i = 0;
+    int x;          //se define una variable para ingresar un numero y otra para hacer la funcion de contador//
+    int i = 0;      //contador inicia en cero//
 
     while (x != 0)
     {
